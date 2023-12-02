4709f2c3cf52d0104c8cd8c06117406f5bd63632
m3brooklyn <m3brooklyn@gmail.com>
2023-12-01 23:12:32 -0800

    mv input -> input.txt

--- Program.cs
+++ Program.cs
@@ -1,9 +1,37 @@
-﻿using System.Collections;
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Collections;
+
+Dictionary<string, Color> colorLookup = new Dictionary<string, Color>();
+colorLookup.Add("red", Color.Red);
+colorLookup.Add("green", Color.Green);
+colorLookup.Add("blue", Color.Blue);
 
 Dictionary<Color, int> constraint = new Dictionary<Color, int>();
 constraint.Add(Color.Red, 12);
 constraint.Add(Color.Green, 13);
 constraint.Add(Color.Blue, 14);
+
+var path = Path.Combine(Directory.GetCurrentDirectory(), "input.txt"); 
+if (File.Exists(path))
+{
+	// Read file using StreamReader. Reads file line by line
+	using StreamReader file = new StreamReader(path);
+	int counter = 0;
+	while (file.ReadLine() is { } ln && counter < 10)
+	{
+		Console.WriteLine("processing line " + counter + " with value " + ln);
+		counter++;
+	}
+	file.Close();
+}
+else
+{
+	Console.WriteLine($"no file found at {path}");
+}
+
+
 enum Color
 {
 	Red, Green, Blue
