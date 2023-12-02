46a61d94ca44ee49122c0659974990bbd60c23b8
m3brooklyn <m3brooklyn@gmail.com>
2023-12-01 23:34:16 -0800

    counting red correctly

--- Program.cs
+++ Program.cs
@@ -19,9 +19,22 @@ if (File.Exists(path))
 	// Read file using StreamReader. Reads file line by line
 	using StreamReader file = new StreamReader(path);
 	int counter = 0;
+	int gameNumber = -1;
+	bool isPossible = false;
+	int sumOfPossibleGameIds;
+	
+	Regex redCubePattern = new Regex(@"(\d+) red", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+	Regex greenCubePattern = new Regex(@"(\d+) green", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+	Regex blueCubePattern = new Regex(@"(\d+) blue", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+	
 	while (file.ReadLine() is { } ln && counter < 10)
 	{
 		Console.WriteLine("processing line " + counter + " with value " + ln);
+		Console.Write("RED: ");
+		foreach (Match match in redCubePattern.Matches(ln))
+		{
+			Console.Write("found " + match.Groups[1].Captures[0] + " at index "+match.Index+", ");
+		}
 		counter++;
 	}
 	file.Close();
