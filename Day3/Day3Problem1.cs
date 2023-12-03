b95f3e754632d1b12b485550d871ccbff6643e30
m3brooklyn <m3brooklyn@gmail.com>
2023-12-02 21:09:32 -0800

    read first 10 lines of file; adding puzzle input.txt

---
+++ Day3/Day3Problem1.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Day3
+{
+	class Day3Problem1
+	{
+		static void Main()
+		{
+			var path = Path.Combine(Directory.GetCurrentDirectory(), "input.txt");
+			if (File.Exists(path))
+			{
+				// Read file using StreamReader. Reads file line by line
+				using StreamReader file = new StreamReader(path);
+				int counter = 0;
+
+				Regex gameIdPattern = new Regex(@"^Game (\d+)", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+				while (file.ReadLine() is { } ln && counter < 10)
+				{
+					Console.WriteLine($"processing line: " + ln);
+
+					counter++;
+				}
+
+				file.Close();
+			}
+			else
+			{
+				Console.WriteLine($"no file found at {path}");
+			}
+		}
+	}
+}
