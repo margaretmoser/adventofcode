776224c6e0b9db0a0cecf328d1f627a7aebc424f
m3brooklyn <m3brooklyn@gmail.com>
2023-12-02 22:24:10 -0800

    Inelegantly finds adjacent symbols on same line as part number, but same logic should work on preceding/following lines with some checking for first/last lines

--- Day3/Day3Problem1.cs
+++ Day3/Day3Problem1.cs
@@ -6,6 +6,8 @@ namespace Day3
 	{
 		static void Main()
 		{
+			List<MatchCollection> symbolMatches = new List<MatchCollection>();
+			
 			var path = Path.Combine(Directory.GetCurrentDirectory(), "input.txt");
 			if (File.Exists(path))
 			{
@@ -13,10 +15,61 @@ namespace Day3
 				using StreamReader file = new StreamReader(path);
 				int counter = 0;
 
-				Regex gameIdPattern = new Regex(@"^Game (\d+)", RegexOptions.Compiled | RegexOptions.IgnoreCase);
-				while (file.ReadLine() is { } ln && counter < 10)
+				/*
+				 * either turn input into a vast 2d array and write a custom comparator, or
+				 * use regexes and match indices which works out to nearly the same thing
+				 *
+				 * 0) what is a symbol? Anything that *isn't* a digit or .
+				 * 1) match any group of digits
+				 * 2) get indices of symbols in line n-1, n, n+1; check if there's a symbol between
+				 *		(index of matched digits-1) and (index of matched digits+match length+1)
+				 */
+
+				Regex symbolPattern = new Regex(@"[^0123456789\.]",
+					RegexOptions.Compiled | RegexOptions.IgnoreCase);
+				Regex partNumberPattern = new Regex(@"(\d+)",
+					RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+				//there is probably a way to do this that only goes through the input file once, but
+				//make it work then make it better
+				while (file.ReadLine() is { } ln && counter < 5)
 				{
 					Console.WriteLine($"processing line: " + ln);
+					foreach (Match match in symbolPattern.Matches(ln))
+					{
+						Console.WriteLine("symbol pattern matched "+match.Value+" at index "+match.Index);
+					}
+					symbolMatches.Add(symbolPattern.Matches(ln));
+
+					counter++;
+				}
+				//restart StreamReader at beginning of file
+				file.DiscardBufferedData();
+				file.BaseStream.Seek(0, SeekOrigin.Begin);
+				counter = 0;
+				int firstIndexToCheckForSymbol = -1;
+				int lastIndexToCheckForSymbol = -1;
+				while (file.ReadLine() is { } ln && counter < 4)
+				{
+					Console.WriteLine($"processing line: " + ln);
+					foreach (Match match in partNumberPattern.Matches(ln))
+					{
+						
+						firstIndexToCheckForSymbol = (Math.Max(0, match.Index - 1));
+						lastIndexToCheckForSymbol = match.Index + match.Value.Length;
+						Console.WriteLine("part number pattern matched "+match.Value+
+						                  $", first index {firstIndexToCheckForSymbol}"+
+						                  $", last index {lastIndexToCheckForSymbol}");
+						//check same line
+						foreach (Match symbolMatch in symbolMatches[counter])
+						{
+							if (symbolMatch.Index >= firstIndexToCheckForSymbol &&
+							    symbolMatch.Index <= lastIndexToCheckForSymbol)
+							{
+								Console.WriteLine("found an adjacent symbol on same line: "+symbolMatch.Value);
+							}
+						}
+					}
 
 					counter++;
 				}
