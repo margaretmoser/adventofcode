7acb17ec4f298b054e33262c8dfc71ad0b1f2f08
m3brooklyn <m3brooklyn@gmail.com>
2024-03-24 20:08:15 -0700

    Preparing files for 9.2

---
+++ Day9/Day9Problem2.cs
@@ -0,0 +1,92 @@
+using System.Text.RegularExpressions;
+namespace Day9;
+
+public class Day9Problem2
+{
+	private List<List<long>> allDataSeries;
+	private long sumOfExtrapolatedValues;
+
+	//I know there is a better way
+	private List<List<long>> derivedLists;
+	
+	public void Run()
+	{
+		allDataSeries = new List<List<long>>();
+		
+		LoadData();
+		ProcessDataSeries();
+	}
+
+	void ProcessDataSeries()
+	{
+		long thisExtrapolatedValue = -1;
+		int lineNumber = 1;
+		foreach (List<long> dataSeries in allDataSeries)
+		{
+			thisExtrapolatedValue = GetExtrapolatedValue(dataSeries);
+			sumOfExtrapolatedValues += thisExtrapolatedValue;
+			Console.WriteLine("for series "+lineNumber+", extrapolated value is "+thisExtrapolatedValue);
+			lineNumber++;
+		}
+		
+		Console.WriteLine("sum of extrapolated values is "+sumOfExtrapolatedValues);
+	}
+	
+	long GetExtrapolatedValue(List<long> thisDataSeries)
+	{
+		bool reachedZero = false;
+		derivedLists = new List<List<long>>();
+		derivedLists.Add(thisDataSeries);
+		List<long> thisList;
+		int index = 0;
+		while (!reachedZero)
+		{
+			thisList = derivedLists[index]
+				.Skip(1)
+				.Zip(derivedLists[index], (current, prev) => current - prev)
+				.ToList();
+			derivedLists.Add(thisList);
+			reachedZero = thisList.Last() == 0;
+			index++;
+		}
+		derivedLists.Reverse();
+		derivedLists[0].Add(0);
+		long sumOfLasts = 0;
+		for (int i = 0; i < derivedLists.Count; i++)
+		{
+			// derivedLists[i].ForEach( x => Console.Write(x+" "));
+			// Console.WriteLine();
+			sumOfLasts += derivedLists[i].Last();
+		}
+		
+		return sumOfLasts;
+	}
+
+
+	void LoadData()
+	{
+		var path = Path.Combine(Directory.GetCurrentDirectory(), "input.txt");
+		Regex dataSeriesPattern = new Regex(@"(-?\d*)",
+			RegexOptions.Compiled | RegexOptions.IgnoreCase);
+		
+		if (File.Exists(path))
+		{
+			using StreamReader file = new StreamReader(path);
+			while (file.ReadLine() is { } ln)
+			{
+				List<long> thisDataSeries = new List<long>();
+				string[] numbersAsStrings = ln.Split(" ");
+				foreach (string s in numbersAsStrings)
+				{
+					Console.Write(s+", ");
+					thisDataSeries.Add(Int32.Parse(s));
+				}
+				Console.WriteLine();
+				allDataSeries.Add(thisDataSeries);
+			}
+			file.Close();
+		}
+		else { Console.WriteLine($"no file found at {path}"); }
+	}
+	
+}
--- Day9/Main.cs
+++ Day9/Main.cs
@@ -1,8 +1,21 @@
 /*
  * https://adventofcode.com/2023/day/9
  * 
- * 
- * 
+ * Extrapolate the last value in a series based on recursively finding the
+ * difference between each element. Like this:
+ *  1   3   6  10  15  21  _28
+ *    2   3   4   5   6   _7
+ *      1   1   1   1   _1
+ *        0   0   0   _0
+ *  where the _s are extrapolated and 28 is the final value.
+ *
+ * Learned about: some LINQ stuff though I didn't end up using much.
+ *
+ * Combining/calculating elements based on previous elements in collection:
+ * https://stackoverflow.com/questions/4460106/how-do-access-previous-item-in-list-using-linq
+ *
+ * The fact that there is no version of ForEach in C# that provides the index as I am used
+ * to from using libraries like UnderscoreJS.
  */
 
 namespace Day9;
@@ -10,10 +23,10 @@ class Day9Main
 {
 	static void Main()
 	{
-		Day9Problem1 problem1 = new Day9Problem1();
-		problem1.Run();
-		// Day8Problem2 problem2 = new Day8Problem2();
-		// problem2.Run();
+		// Day9Problem1 problem1 = new Day9Problem1();
+		// problem1.Run();
+		Day9Problem2 problem2 = new Day9Problem2();
+		problem2.Run();
 	}
 			
 }
