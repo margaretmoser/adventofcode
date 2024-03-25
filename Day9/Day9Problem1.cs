ce5ae8576f52b0b9ff0c5410583558110423563a
m3brooklyn <m3brooklyn@gmail.com>
2024-03-24 19:57:24 -0700

    Naive and unsatisfying solution to 9.1. Have a feeling I'll need to redo it recursively for Part 2

--- Day9/Day9Problem1.cs
+++ Day9/Day9Problem1.cs
@@ -4,14 +4,65 @@ namespace Day9;
 public class Day9Problem1
 {
 	private List<List<long>> allDataSeries;
+	private long sumOfExtrapolatedValues;
+
+	//I know there is a better way
+	private List<List<long>> derivedLists;
 	
 	public void Run()
 	{
 		allDataSeries = new List<List<long>>();
+		
 		LoadData();
+		ProcessDataSeries();
 	}
 
+	void ProcessDataSeries()
+	{
+		long thisExtrapolatedValue = -1;
+		int lineNumber = 1;
+		foreach (List<long> dataSeries in allDataSeries)
+		{
+			thisExtrapolatedValue = GetExtrapolatedValue(dataSeries);
+			sumOfExtrapolatedValues += thisExtrapolatedValue;
+			Console.WriteLine("for series "+lineNumber+", extrapolated value is "+thisExtrapolatedValue);
+			lineNumber++;
+		}
+		
+		Console.WriteLine("sum of extrapolated values is "+sumOfExtrapolatedValues);
+	}
 	
+	long GetExtrapolatedValue(List<long> thisDataSeries)
+	{
+		bool reachedZero = false;
+		derivedLists = new List<List<long>>();
+		derivedLists.Add(thisDataSeries);
+		List<long> thisList;
+		int index = 0;
+		while (!reachedZero)
+		{
+			thisList = derivedLists[index]
+				.Skip(1)
+				.Zip(derivedLists[index], (current, prev) => current - prev)
+				.ToList();
+			derivedLists.Add(thisList);
+			reachedZero = thisList.Last() == 0;
+			index++;
+		}
+		derivedLists.Reverse();
+		derivedLists[0].Add(0);
+		long sumOfLasts = 0;
+		for (int i = 0; i < derivedLists.Count; i++)
+		{
+			// derivedLists[i].ForEach( x => Console.Write(x+" "));
+			// Console.WriteLine();
+			sumOfLasts += derivedLists[i].Last();
+		}
+		
+		return sumOfLasts;
+	}
+
+
 	void LoadData()
 	{
 		var path = Path.Combine(Directory.GetCurrentDirectory(), "input.txt");
