deade04d6b6ed6b5fe39e166a603ae478aa95c72
m3brooklyn <m3brooklyn@gmail.com>
2024-04-13 11:47:20 -0700

    Initial checkin of 10.2, just a copy of 10.1 for the moment. Notes on reasoning added in a comment to Main

---
+++ Day10/Day10Problem2.cs
@@ -0,0 +1,198 @@
+namespace Day10;
+public class Day10Problem2
+
+/* just a copy of Problem1 for the moment */
+
+{
+	private char[,] rawMap;
+	private Day10Main.Coords startPosition;
+	private Day10Main.Coords currentPosition;
+	
+	LinkedList<char> linkedMap = new LinkedList<char>();
+	
+	public void Run()
+	{
+		LoadData();
+		TraverseMap();
+	}
+
+
+	//Naive and extremely verbose solution, but it works
+	void TraverseMap()
+	{
+		char currentChar;
+		bool cycleFound = false;
+		bool endFound = false;
+		int pathLength = 0;
+		currentPosition = startPosition;
+		//forcing right for initial test; come back and iterate over up, down, left
+		Direction currentDirection = Direction.Up;
+		currentPosition.Y--;
+		
+		Console.WriteLine("current position is "+currentPosition);
+		while (!cycleFound)
+		{
+			currentChar = rawMap[currentPosition.X, currentPosition.Y];
+			Console.Write(currentChar);
+			pathLength++;
+			if (currentChar == 'S')
+			{
+				Console.WriteLine("back to the beginning; cycle length is "+pathLength+
+				                  " and most distant point is "+pathLength/2+" steps away");
+				cycleFound = true;
+				break;
+			} else if (currentChar == '.')
+			{
+				Console.WriteLine("pipe end found, should exit loop and start again from S");
+				endFound = true;
+			}
+			else
+			{
+				if (currentDirection == Direction.Up)
+				{
+					switch (currentChar)
+					{
+						case '|': //move up
+							currentPosition.Y -= 1;
+							currentDirection = Direction.Up;
+							break;
+						case 'F': //move right
+							currentPosition.X += 1;
+							currentDirection = Direction.Right;
+							break;
+						case '7': //move left
+							currentPosition.X -= 1;
+							currentDirection = Direction.Left;
+							break;
+						default:
+							Console.WriteLine("pipe end found, should exit loop and start again from S");
+							endFound = true;
+							break;
+					}
+				}
+				else if (currentDirection == Direction.Right)
+				{
+					switch (currentChar)
+					{
+						case '-': //move right
+							currentPosition.X += 1;
+							currentDirection = Direction.Right;
+							break;
+						case '7': //move down
+							currentPosition.Y += 1;
+							currentDirection = Direction.Down;
+							break;
+						case 'J': //move up
+							currentPosition.Y -= 1;
+							currentDirection = Direction.Up;
+							break;
+						default:
+							Console.WriteLine("pipe end found, should exit loop and start again from S");
+							endFound = true;
+							break;
+					}
+				}
+				else if (currentDirection == Direction.Down)
+				{
+					switch (currentChar)
+					{
+						case '|': //move down
+							currentPosition.Y += 1;
+							currentDirection = Direction.Down;
+							break;
+						case 'L': //move right
+							currentPosition.X += 1;
+							currentDirection = Direction.Right;
+							break;
+						case 'J': //move left
+							currentPosition.X -= 1;
+							currentDirection = Direction.Left;
+							break;
+						default:
+							Console.WriteLine("pipe end found, should exit loop and start again from S");
+							endFound = true;
+							break;
+					}
+				}
+				else if (currentDirection == Direction.Left)
+				{
+					switch (currentChar)
+					{
+						case '-': //move left
+							currentPosition.X -= 1;
+							currentDirection = Direction.Left;
+							break;
+						case 'F': //move down
+							currentPosition.Y += 1;
+							currentDirection = Direction.Down;
+							break;
+						case 'L': //move up
+							currentPosition.Y -= 1;
+							currentDirection = Direction.Up;
+							break;
+						default:
+							Console.WriteLine("pipe end found, should exit loop and start again from S");
+							endFound = true;
+							break;
+					}
+				}
+			}
+
+			if (endFound)
+			{
+				currentPosition = startPosition;
+				switch (currentDirection)
+				{
+					case Direction.Up:
+						currentDirection = Direction.Right;
+						currentPosition.X++;
+						break;
+					case Direction.Right:
+						currentDirection = Direction.Down;
+						currentPosition.Y++;
+						break;
+					case Direction.Down:
+						currentDirection = Direction.Left;
+						currentPosition.X--;
+						break;
+					case Direction.Left:
+						Console.WriteLine("error, have already checked all directions");
+						break;
+				}
+				endFound = false;
+			}
+		}
+	}
+	
+	
+	void LoadData()
+	{
+		var path = Path.Combine(Directory.GetCurrentDirectory(), "input.txt");
+		
+		if (File.Exists(path))
+		{
+			
+			string[] textAsLines = File.ReadAllLines(path);
+			int lineLength = textAsLines[0].Length;
+			rawMap = new char[textAsLines.Length, lineLength];
+			int i = 0;
+			foreach (string inputLine in textAsLines)
+			{
+				int j = 0;
+				foreach (char c in inputLine)
+				{
+					rawMap[j,i] = c;
+					if (c == 'S')
+					{
+						startPosition = new Day10Main.Coords(j, i);
+					}
+					j++;
+				}
+				i++;
+			}
+			Console.WriteLine("start position is "+startPosition.ToString());
+		}
+		else { Console.WriteLine($"no file found at {path}"); }
+	}
+	
+}
--- Day10/Main.cs
+++ Day10/Main.cs
@@ -1,10 +1,25 @@
 /*
  * https://adventofcode.com/2023/day/10
  * 
- * Read in an undirected, unweighted graph with one cycle; find the cycle given its starting point;
+ * For Day 1, read in an undirected, unweighted graph with one cycle; find the cycle given its starting point;
  * find the farthest point included in the cycle and return the number of steps from
- * starting point to farthest point (half the length of the cycle).
+ * starting point to farthest point (half the length of the cycle). I thought this would require
+ * actually constructing the graph, but found a naive solution that just required a 2d array and
+ * tracking the direction of "movement" along the path. It could probably be optimized by checking
+ * character pairs rather than the massive switch statement.
  *
+ * In Day 2, find the number of tiles enclosed by the loop (cycle). A starting point, indirectly suggested
+ * by the problem, might be a kind of simple convex-hull approach: find the cycle, then start from the edges
+ * of the graph and count inwards to find the tiles *not* enclosed by the path; then the enclosed tiles would
+ * just be the ones that weren't part of the loop or outside of it. But the loop can have "holes" like this:
+ * L7L7																									F-JI
+ * -JOL		<- hole that is outside the loop (O)					L7IF		<- hole that is inside the loop (I)
+ * OOF-																									O|FJ
+ * 
+ * The next theory to try out is counting odd/even numbers of tiles that are between a given tile and
+ * an edge of the map. Drawing this out has proven difficult.
+ * 
+ * 
  * 
  * Learned about:
  *      (reminder: comma and plus do different things in Console Write/WriteLine)
